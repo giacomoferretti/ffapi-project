3c64dbe1395f70469a4bdd16e567f4812101f254
Giacomo Ferretti <me@hexile.xyz>
2019-06-02 19:33:31 +0200

    Added callback handler

--- commands/promo.py
+++ commands/promo.py
@@ -13,10 +13,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import logging
 
 from mcdapi import promo
 from telegram import Update, ParseMode
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, run_async
 
 from commands import base
 
@@ -24,7 +25,19 @@ from commands import base
 class PromoCommand(base.Command):
     name = 'promo'
 
+    @run_async
     def handler(self, update: Update, context: CallbackContext):
         if self.can_run(update, context):
-            body = self.__config__.get_template('promo.md').format(promo.generate_random_promocode())
-            context.bot.send_message(chat_id=update.effective_chat.id, text=body, parse_mode=ParseMode.MARKDOWN)
+            body = self.__config__.get_template('promo.html').format(promo.generate_random_promocode())
+            context.bot.send_message(chat_id=update.effective_chat.id, text=body, parse_mode=ParseMode.HTML)
+
+    @run_async
+    def callback_handler(self, update: Update, context: CallbackContext):
+        __logger__ = logging.getLogger(__name__)
+
+        query = update.callback_query
+
+        if self.check_callback(query.data, 'generate'):
+            body = self.__config__.get_template('promo.html').format(promo.generate_random_promocode())
+            context.bot.answer_callback_query(query.id, text='Promocode generato.')
+            context.bot.send_message(chat_id=update.effective_chat.id, text=body, parse_mode=ParseMode.HTML)
